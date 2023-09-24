bf575fc576170a8fe9cde7805af5973a101817de
Fahad Ali <134847376+FaddyWho@users.noreply.github.com>
2023-09-24 21:47:22 +0500

    Update script.js
    added comments

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const form = document.getElementById('theForm');
+const form = document.getElementById('theForm'); // get the form element from which we need data
 
-form.addEventListener('submit', callbackFunction);
+form.addEventListener('submit', callbackFunction); // event listener for the submit to use the callback function
                             
 function callbackFunction(event) {
-    event.preventDefault();
-    const myFormData = new FormData(event.target);
+    event.preventDefault(); // prevent default refresh
+    const myFormData = new FormData(event.target); // get the data
         
-    const formDataObj = Object.fromEntries(myFormData.entries());
+    const formDataObj = Object.fromEntries(myFormData.entries()); //put the data inside an object
     console.log(formDataObj)
         
 };
